9256b8b2c9fe258d51afa8cf1af13b26cdff74ac
lz <438106327@qq.com>
2019-11-13 19:47:20 +0800

    this project bitcoinRest finish

--- bitcoinexplorer1113/src/main/java/com/lz/bitcoinexplorer1113/Bitcoinexplorer1113Application.java
+++ bitcoinexplorer1113/src/main/java/com/lz/bitcoinexplorer1113/Bitcoinexplorer1113Application.java
@@ -3,9 +3,11 @@ package com.lz.bitcoinexplorer1113;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("com.lz.bitcoinexplorer.dao")
+@EnableFeignClients
 public class Bitcoinexplorer1113Application {
 
     public static void main(String[] args) {
--- bitcoinexplorer1113/src/main/java/com/lz/bitcoinexplorer1113/client/JsonRpcBitcoinClient.java
+++ bitcoinexplorer1113/src/main/java/com/lz/bitcoinexplorer1113/client/JsonRpcBitcoinClient.java
@@ -1,4 +1,25 @@
 package com.lz.bitcoinexplorer1113.client;
 
+import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Base64;
+import java.util.HashMap;
+
 public class JsonRpcBitcoinClient {
+    private JsonRpcHttpClient jsonRpcHttpClient;
+
+    public JsonRpcBitcoinClient() throws MalformedURLException {
+        String s = Base64.getEncoder().encodeToString(("lz" + ":" + "123456").getBytes());
+        HashMap<String, String> ma = new HashMap<>();
+        ma.put("Authorization","Basic "+s);
+        jsonRpcHttpClient=new JsonRpcHttpClient(new URL("http://localhost:18332"),ma);
+
+    }
+
+    public String getBestBlockhash() throws Throwable {
+        String bestblockhash = jsonRpcHttpClient.invoke("getbestblockhash", new Object[]{}, String.class);
+        return bestblockhash;
+    }
 }
