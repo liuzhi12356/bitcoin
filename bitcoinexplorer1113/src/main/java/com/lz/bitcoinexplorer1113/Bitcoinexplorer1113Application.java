ae2b9898179515bdbaf75015864d8c443e8b09b8
lz <438106327@qq.com>
2019-11-13 19:14:41 +0800

    this project init

--- bitcoinexplorer1113/src/main/java/com/lz/bitcoinexplorer1113/Bitcoinexplorer1113Application.java
+++ bitcoinexplorer1113/src/main/java/com/lz/bitcoinexplorer1113/Bitcoinexplorer1113Application.java
@@ -1,9 +1,11 @@
 package com.lz.bitcoinexplorer1113;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lz.bitcoinexplorer.dao")
 public class Bitcoinexplorer1113Application {
 
     public static void main(String[] args) {
