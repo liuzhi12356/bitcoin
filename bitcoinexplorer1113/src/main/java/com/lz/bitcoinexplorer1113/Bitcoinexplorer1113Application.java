749e378fda44bafe50fd774c58365b6045f810bc
lz <438106327@qq.com>
2019-11-13 18:56:48 +0800

    this project pom.xml finish

---
+++ bitcoinexplorer1113/src/main/java/com/lz/bitcoinexplorer1113/Bitcoinexplorer1113Application.java
@@ -0,0 +1,13 @@
+package com.lz.bitcoinexplorer1113;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bitcoinexplorer1113Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bitcoinexplorer1113Application.class, args);
+    }
+
+}
---
+++ bitcoinexplorer1113/src/test/java/com/lz/bitcoinexplorer1113/Bitcoinexplorer1113ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lz.bitcoinexplorer1113;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bitcoinexplorer1113ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
